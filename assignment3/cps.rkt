72716752d6134440bc97cc45d2d8bd4941fb10bf
Greg Van Aken <gavanaken@gmail.com>
2018-11-07 13:05:34 -0500

    Phase 1: Most of boxify done, some of alphatize done

--- assignment3/cps.rkt
+++ assignment3/cps.rkt
@@ -38,12 +38,42 @@
            
            ; .. all all other forms in the language ...
 
+           [`(let ([,xs ,es] ...) ,e0)
+            `(let ,(map (lambda (x e)
+                          (if (memv x mutated-vars)
+                              `(,x (prim make-vector '1 ,(boxify e)))
+                              `(,x ,(boxify e))))xs es) ; map making a vector over anything that needs it - recurse on e's
+               ,(boxify e0))] ; finally, recurse on the let body
+           [`(lambda ,x ,e0)
+            `(lambda ,x
+               ,(if (memv x mutated-vars)
+                    `(let ([,x (prim make-vector '1 ,x)]) ,(boxify e0)) ; make a vector out of the vars - recurse on e's
+                    (boxify e0)))] ; finally, recurse on the lambda body
+           [`(lambda (,x ...) ,e)
+            "not sure"]
+           [`(apply ,e0 ,e1)
+            `(apply ,(boxify e0)
+                    ,(boxify e1))] ; just box the e's
+           [`(,es ...)
+            (map boxify es)] ; just boxify over all e's
+           [`(prim ,op ,es ...)
+            `(prim ,op ,@(map boxify es))] ; I think we need to splice the map results (from desugar.rkt)
+           [`(apply-prim ,op ,e0)
+            `(apply-prim ,op ,(boxify e0))] ; just box e0
+           [`(if ,e0 ,e1 ,e2)
+            `(if ,(box e0) ,(box e1) ,(box e2))] ; just box the e's
            [`(set! ,x ,e0)
             `(prim vector-set! ,x '0 ,(boxify e0))]
+           [`(set! ,x ,e0)
+            `(prim vector-set! ,x '0 ,(boxify e0))] ; from SSA slides - being sure to box the e
+           [`(call/cc ,e0)
+            `(call/cc ,(boxify e0))] ; just box e0
            [(? symbol? x)
             (if (set-member? mutated-vars x)
                 `(prim vector-ref ,x '0)
-                x)]))
+                x)]
+           [`',dat `',dat] ; dat is dat
+           ))
   (boxify e))
 
 
@@ -74,6 +104,16 @@
             (define env+ (foldl (lambda (x x+ env) (hash-set env x x+)) env xs xs+))
             `(lambda ,xs+ ,((rename env+) e0))]
            ; etc ...
+           [`(let ([,xs ,es] ...) ,e0)
+            (define xs+ (map gensym xs))
+            (define env+ (foldl (lambda (x x+ env) (hash-set env x x+)) env xs xs+))
+            "what is rest"]
+           [`(lambda ,x ,e0)
+            (define x+ (gensym x))
+            (define env+ (hash-set env x x+))
+            `(lambda ,x+ ,((rename env+) e0))]
+           [`(prim ,op ,es ...)
+            `(prim ,op ,@(map (rename env) es))] ; splice together mapping rename over the e's
            ))
   ((rename (hash)) e))
 
